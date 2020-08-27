077e436ca4bc0846c9206e5d3c08fb86d0e637df
Joseph Haaga <haaga.joe@gmail.com>
2020-08-27 10:29:03 -0400

    render simple network graph

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,10 +9,74 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+import Graph from "react-graph-vis";
+import {
+  Alignment,
+  Button,
+  Classes,
+  H5,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  NavbarHeading,
+  Switch,
+} from "@blueprintjs/core";
+
+import "@blueprintjs/core/lib/css/blueprint.css";
+
 export default function HomePage() {
+  const graph = {
+    nodes: [
+      { id: 1, label: "Node 1", title: "node 1 tootip text" },
+      { id: 2, label: "Node 2", title: "node 2 tootip text" },
+      { id: 3, label: "Node 3", title: "node 3 tootip text" },
+      { id: 4, label: "Node 4", title: "node 4 tootip text" },
+      { id: 5, label: "Node 5", title: "node 5 tootip text" }
+    ],
+    edges: [
+      { from: 1, to: 2 },
+      { from: 1, to: 3 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 }
+    ]
+  };
+ 
+  const options = {
+    layout: {
+      hierarchical: true
+    },
+    edges: {
+      color: '#000000',
+    },
+    height: '500px',
+  };
+
+  const events = {
+    select: function(event) {
+      var { nodes, edges } = event;
+    }
+  };
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <div>
+      <Navbar>
+        <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>Blueprint</Navbar.Heading>
+            <Navbar.Divider />
+            <Button className="bp3-minimal" icon="home" text="Home" />
+            <Button className="bp3-minimal" icon="document" text="Files" />
+        </Navbar.Group>
+    </Navbar>
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
+      <Graph
+        graph={graph}
+        options={options}
+        events={events}
+        getNetwork={network => {
+          //  if you want access to vis.js network api you can set the state in a parent component using this property
+        }}
+      />
+    </div>
   );
 }
